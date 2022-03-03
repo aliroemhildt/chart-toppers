4c7db9d3d1b0f8b9abed4ad8e53b8716f9a59d94
Ali Roemhildt <aliroemhildt@gmail.com>
2022-03-03 12:43:47 -0700

    Start building out app logic with hooks

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
-import logo from './logo.svg';
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { getSongs } from './apiCalls';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      src: ''
-    }
-  }
+const App = () => {
+  const [allSongs, setAllSongs] = useState([]);
+  const [selectedQuiz, setSelectedQuiz] = useState('');
 
-  componentDidMount = () => {
+  const getAllSongs = async () => {
     getSongs()
-      .then(data => this.setState({src: data[2021].song1.image_url}))
-  }  
-
-  render = () => {
-    return (
-      <div className="App">
-        <header className="App-header">
-          {this.state.src && <img src={this.state.src} />}
-        </header>
-      </div>
-    );
+      .then(data => setAllSongs)
   }
+
+  return (
+    <div className="App">
+      <p>Test</p>
+    </div>
+  );
 }
 
 export default App;
