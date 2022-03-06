d468595d7c75b42e7839a3d1f75925d999c3599b
Ali Roemhildt <aliroemhildt@gmail.com>
2022-03-05 23:37:19 -0700

    Add tests for home page

--- cypress/integration/home_page.spec.js
+++ cypress/integration/home_page.spec.js
@@ -1,3 +1,10 @@
 describe('Home page', () => {
-  
+  it('Should show quiz options and page info on home page', () => {
+    cy.intercept('GET', 'https://chart-toppers-api.herokuapp.com/api/v1/songs', { fixture: 'sampleSongData.json' })
+    cy.visit('http://localhost:3000')
+
+    cy.get('.page-title').contains('Chart Toppers')
+      .get('.quiz-button').should('have.text', '1980\'s1990\'s2000\'s2010\'s')
+      .
+  })
 })
