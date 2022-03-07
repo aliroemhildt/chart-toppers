cea94a0c241a98fafc8f0d9d5acf8ea360190ee9
Ali Roemhildt <aliroemhildt@gmail.com>
2022-03-06 17:26:24 -0700

    Style footer and complete styling for home page

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,9 +3,11 @@ import './Footer.scss';
 const Footer = () => {
   return (
     <footer>
-      <p>Created by Ali Roemhildt</p>
-      <a className='footer-link linkedin' href='https://www.linkedin.com/in/aliroemhildt/'>LinkedIn</a>
-      <a className='footer-link github' href='https://github.com/aliroemhildt'>GitHub</a>
+      <p className='created-by'>Created by Ali Roemhildt</p>
+      <div className='footer-links-container'>
+        <a className='footer-link linkedin' href='https://www.linkedin.com/in/aliroemhildt/'>LinkedIn</a>
+        <a className='footer-link github' href='https://github.com/aliroemhildt'>GitHub</a>
+      </div>
     </footer>
   )
 }
